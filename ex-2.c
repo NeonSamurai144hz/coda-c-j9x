e5d6cf5cb8cf0cdd12f5f3183d9ff4058ee763aa
Bobeica Nikita <nbobeica@DESKTOP-44P6421>
2024-10-08 15:11:20 +0200

    EXO FINI

---
+++ ex-2.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>  
+#include "struct.h"
+
+void fill_struct(character * perso)
+{
+perso-> wisdom = 35;
+perso-> agility = 14;
+perso-> strenght = 82;
+perso-> dexterity = 25;
+perso-> intelligence = 22;
+perso-> endurance = 55;
+perso-> vigor = 55;
+perso-> faith = 20;
+}
+int main()  
+{  
+    character perso;  
+    fill_struct(&perso);  
+  
+  printf("Your character has:  %d vigor  %d agility  %d endurance  %d strenght  %d dexterity  %d intelligence  %d wisdom  %d faith\n", perso.vigor, perso.agility, perso.endurance, perso.strenght, perso.dexterity, perso.intelligence, perso.wisdom, perso.faith );
+
+exit(0);
+}
---
+++ struct.h
@@ -0,0 +1,17 @@
+#ifndef TESTS_STRUCT_H  
+#define TESTS_STRUCT_H  
+  
+
+  struct s_char 
+  {
+    int strenght;
+    int dexterity;
+    int intelligence;
+    int agility;
+    int endurance;
+    int wisdom;
+    int faith;
+    int vigor;
+  };
+  typedef struct s_char character;
+  #endif
